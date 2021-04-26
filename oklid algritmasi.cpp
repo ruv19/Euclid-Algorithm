3d5eda5fe050b08bbde6ef8f8a1f2f7d5123e84a
Rüveyda Dal <71987230+ruv19@users.noreply.github.com>
2021-04-26 16:35:46 +0300

    Add files via upload

---
+++ oklid algritmasi.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (void)
+
+{
+
+    int m, n; /* given numbers */
+
+    clrscr();
+
+    printf("Enter-two integer numbers: ");
+
+    scanf ("%d %d", &m, &n);
+
+    while (n > 0)
+
+    {
+
+        int r = m % n;
+
+        m = n;
+
+        n = r;
+
+    }
+
+    printf ("GCD = %d \n",m);
+
+    getch();
+
+}
