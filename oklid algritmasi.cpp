2a6ba7030acf0c039230e1fa48899f1ad43c1c01
Rüveyda Dal <ruveydadal.3@gmail.com>
2022-08-12 10:40:03 +0300

    Update oklid algritmasi.cpp

--- oklid algritmasi.cpp
+++ oklid algritmasi.cpp
@@ -1,31 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main (void)
 
-{
+int main (void) {
 
-    int m, n; /* given numbers */
-
+    int m, n;
     clrscr();
-
     printf("Enter-two integer numbers: ");
-
     scanf ("%d %d", &m, &n);
-
-    while (n > 0)
-
-    {
+    while (n > 0) {
 
         int r = m % n;
-
         m = n;
-
         n = r;
-
     }
-
     printf ("GCD = %d \n",m);
-
     getch();
-
 }
